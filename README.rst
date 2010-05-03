11e96a2c03dce651ad44831c9847135d2573afdd
Jared Forsyth <jared@jaredforsyth.com>
2010-05-03 16:18:41 -0400

    initial commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+Django Colorfield
+---------------------
+
+This module fills the need of having a 'colorfield' that's usable in both
+django models and forms.
+
+Makes use of http://jscolor.com/.
+
---
+++ colorfield/fields.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import re
+
+from django import forms
+from django.db import models
+from django.conf import settings
+from django.core.validators import RegexValidator
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+
+color_re = re.compile('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
+validate_color = RegexValidator(color_re, _(u'Enter a valid color.'), 'invalid')
+
+class ColorWidget(forms.Widget):
+    class Media:
+        js = [settings.MEDIA_URL + 'colorfield/jscolor.js']
+        
+    def render(self, name, value, attrs=None):
+        return render_to_string('colorfield/color.html', locals())
+        
+class ColorField(models.CharField):
+    default_validators = [validate_color]
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 10
+        super(ColorField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        kwargs['widget'] = ColorWidget
+        return super(ColorField, self).formfield(**kwargs)
+
+# vim: et sw=4 sts=4
