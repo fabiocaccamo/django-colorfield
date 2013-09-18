b16f59a697ee3dd27eb043781186cdbb6c4b0a72
Jared Forsyth <jared@jaredforsyth.com>
2013-09-18 11:51:12 -0600

    adding manifest, updating setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import os
 from distutils.core import setup
 
-version = '0.1'
+version = '0.1.1'
 
 setup(
     name="django-colorfield",
     version=version,
     keywords=["django", "color"],
+    author='Jared Forsyth',
+    author_email='jared@jaredforsyth.com',
+    license='MIT',
     long_description="A small app providing a colorpicker field for django",
     description="A small app providing a colorpicker field for django",
-    license="Apache Software License",
     classifiers=[
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
     ],
     packages=['colorfield'],
     install_requires=['django>=1.2'],
