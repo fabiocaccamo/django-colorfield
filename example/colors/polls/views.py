6b851aa24c506b2b139cc879422c4c36f20c70b5
vsoch <vsochat@stanford.edu>
2017-12-09 10:32:40 -0800

    modified:   colors/polls/views.py

--- example/colors/polls/views.py
+++ example/colors/polls/views.py
@@ -33,21 +33,13 @@ class ResultsView(generic.DetailView):
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['color'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the poll voting form.
-        return render(request, 'polls/detail.html', {
-            'poll': p,
-            'error_message': "You didn't select a color!",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('results', args=(p.id,)))
+    choice, created = p.choice_set.get_or_create(color=request.POST['color'])
+    choice.votes += 1
+    choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('results', args=(p.id,)))
 
 
 def create_polls(request):
