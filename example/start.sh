f397c13ca63554ff7210d12e7bfdd5de240a24b9
vsoch <vsochat@stanford.edu>
2017-12-09 09:34:01 -0800

    updating template

--- example/start.sh
+++ example/start.sh
@@ -2,7 +2,6 @@
 
 python3 /code/manage.py makemigrations admin
 python3 /code/manage.py makemigrations polls
-python3 /code/manage.py makemigrations colorfield
 python3 /code/manage.py migrate
 python3 /code/manage.py collectstatic --noinput
 python3 /code/manage.py runserver 0.0.0.0:8000
