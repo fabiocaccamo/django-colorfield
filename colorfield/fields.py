aaaf09b528c2a1886c67e5c48be1a1eca55ad3c2
Romain Catajar <rcatajar@gmail.com>
2021-01-13 17:52:03 +0100

    Variable naming and wordings

--- colorfield/fields.py
+++ colorfield/fields.py
@@ -14,18 +14,18 @@ else:
 
 COLOR_HEX_RE = re.compile('^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
 color_hex_validator = RegexValidator(
-    COLOR_HEX_RE, _('Enter a valid color.'), 'invalid')
+    COLOR_HEX_RE, _('Enter a valid hex color, eg. #000000'), 'invalid')
 
 COLOR_HEXA_RE = re.compile('#([A-Fa-f0-9]{8}|[A-Fa-f0-9]{4})$')
 color_hexa_validator = RegexValidator(
-    COLOR_HEXA_RE, _('Enter a valid color.'), 'invalid')
+    COLOR_HEXA_RE, _('Enter a valid color, eg. #00000000'), 'invalid')
 
 VALIDATORS_PER_FORMAT = {
     'hex': color_hex_validator,
     'hexa': color_hexa_validator
 }
 
-WHITE_IN_FORMAT = {
+DEFAULT_PER_FORMAT = {
     'hex': '#FFFFFF',
     'hexa': '#FFFFFFFF'
 }
@@ -36,12 +36,11 @@ class ColorField(models.CharField):
     default_validators = []
 
     def __init__(self, *args, **kwargs):
-        color_format = kwargs.pop('color_format', 'hex')
         self.palette = kwargs.pop('palette', [])
-        if color_format not in ['hex', 'hexa']:
-            raise ValueError('Unsupported color format: {}'.format(color_format))
-        self.color_format = color_format
-        self.default_validators = [VALIDATORS_PER_FORMAT[self.color_format]]
+        self.format = kwargs.pop('format', 'hex').lower()
+        if self.format not in ['hex', 'hexa']:
+            raise ValueError('Unsupported color format: {}'.format(self.format))
+        self.default_validators = [VALIDATORS_PER_FORMAT[self.format]]
 
         kwargs.setdefault('max_length', 18)
         if kwargs.get('null'):
@@ -50,13 +49,13 @@ class ColorField(models.CharField):
         elif kwargs.get('blank'):
             kwargs.setdefault('default', '')
         else:
-            kwargs.setdefault('default', WHITE_IN_FORMAT[self.color_format])
+            kwargs.setdefault('default', DEFAULT_PER_FORMAT[self.format])
         super(ColorField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
         kwargs['widget'] = ColorWidget(attrs={
             'default': self.get_default(),
-            'color_format': self.color_format,
+            'format': self.format,
             'palette': self.palette,
         })
         return super(ColorField, self).formfield(**kwargs)
