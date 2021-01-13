a8d58e5db8f824a51b93f739b82f218ba162c880
Romain Catajar <rcatajar@gmail.com>
2021-01-13 14:06:11 +0100

    Upgrade jscolor to 2.4.5

--- colorfield/static/colorfield/colorfield.js
+++ colorfield/static/colorfield/colorfield.js
@@ -1,12 +1,12 @@
 /** global: django */
 
-window.addEventListener('load', function(event) {
-    if (typeof(django) !== 'undefined' && typeof(django.jQuery) !== 'undefined') {
-        (function($) {
-            // add colopicker to inlines added dynamically
-            $(document).on('formset:added', function onFormsetAdded(event, row) {
-                jscolor.installByClassName('jscolor');
-            });
-        })(django.jQuery);
-    }
+window.addEventListener("load", function (event) {
+  if (typeof django !== "undefined" && typeof django.jQuery !== "undefined") {
+    (function ($) {
+      // add colopicker to inlines added dynamically
+      $(document).on("formset:added", function onFormsetAdded(event, row) {
+        jscolor.install();
+      });
+    })(django.jQuery);
+  }
 });
