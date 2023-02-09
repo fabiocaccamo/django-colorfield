e512f2f4b261f1d65679d949b0aeb4006778fccf
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-02-09 19:13:27 +0100

    Add `metadata` module and read package attrs dynamically.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,11 +1,34 @@
+import re
+
 from django.test import TestCase
 
-from colorfield.version import __version__
+from colorfield.metadata import (
+    __author__,
+    __copyright__,
+    __description__,
+    __email__,
+    __license__,
+    __title__,
+    __version__,
+)
 
 
-class ColorFieldMetadataTestCase(TestCase):
-    def setUp(self):
-        pass
+class MetadataTestCase(TestCase):
+    """
+    This class describes a metadata test case.
+    """
 
-    def tearDown(self):
-        pass
+    def test_metadata(self):
+        self.assertTrue(isinstance(__author__, str))
+        self.assertTrue(isinstance(__copyright__, str))
+        self.assertTrue(isinstance(__description__, str))
+        self.assertTrue(isinstance(__email__, str))
+        self.assertTrue(isinstance(__license__, str))
+        self.assertTrue(isinstance(__title__, str))
+        self.assertTrue(isinstance(__version__, str))
+
+    def test_version(self):
+        v = __version__
+        v_re = re.compile(r"^([0-9]+)(\.([0-9]+)){1,2}$")
+        v_match = v_re.match(v)
+        self.assertTrue(v_match is not None)
