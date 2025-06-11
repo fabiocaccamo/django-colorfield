2cdab7162f2b421a5de7d04f986f410c9cfb5d6b
Fabio Caccamo <fabio.caccamo@gmail.com>
2025-06-12 00:20:01 +0200

    Refactor `ColorAdminMixin`.

--- colorfield/admin.py
+++ colorfield/admin.py
@@ -2,29 +2,27 @@ from colorfield.fields import ColorField
 
 
 class ColorAdminMixin:
-    add_form_template = "colorfield/admin/change_form.html"
-    change_form_template = "colorfield/admin/change_form.html"
+    # add_form_template = "colorfield/admin/change_form.html"
+    # change_form_template = "colorfield/admin/change_form.html"
     change_list_template = "colorfield/admin/change_list.html"
 
-    def get_color_fields(self, request, object_id=None):
+    def _get_color_fields(self):
         return [
-            (f.name, f.choices)
-            for f in self.model._meta.fields
-            if isinstance(f, ColorField)
+            (field.name, field.choices)
+            for field in self.model._meta.fields
+            if isinstance(field, ColorField)
         ]
 
-    def _get_extra_content(self, request, object_id=None, extra_context=None):
-        fields = self.get_color_fields(request, object_id)
+    def _get_extra_context(self, extra_context):
+        fields = self._get_color_fields()
         extra_context = extra_context or {}
         extra_context.update({"colorfield_list_of_fields": fields})
         return extra_context
 
     def changelist_view(self, request, extra_context=None):
-        extra_context = self._get_extra_content(request, extra_context=extra_context)
+        extra_context = self._get_extra_context(extra_context)
         return super().changelist_view(request, extra_context)
 
     def changeform_view(self, request, object_id=None, form_url="", extra_context=None):
-        extra_context = self._get_extra_content(
-            request, object_id, extra_context=extra_context
-        )
+        extra_context = self._get_extra_context(extra_context)
         return super().changeform_view(request, object_id, form_url, extra_context)
